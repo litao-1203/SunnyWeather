f4d35802fa8c3961c67be1a28ba2786fe02269d3
litao_1203 <1328336954@qq.com>
2021-04-23 10:40:34 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -1,9 +1,14 @@
 package com.example.sunnyweather.logic
 
 import androidx.lifecycle.liveData
+import com.example.sunnyweather.logic.dao.PlaceDao
 import com.example.sunnyweather.logic.model.Place
+import com.example.sunnyweather.logic.model.Weather
 import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
 
@@ -21,4 +26,47 @@ object Repository {
         }
         emit(result)
     }
+
+    fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO){
+        val result = try {
+            coroutineScope {
+                val deferredRealtime = async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status == "ok"){
+                    val weather = Weather(realtimeResponse.result.realtime, dailyResponse.result.daily)
+                    Result.success(weather)
+                }else{
+                    Result.failure(
+                            RuntimeException(
+                                    "realtime response status is ${realtimeResponse.status}"
+                            )
+                    )
+                }
+            }
+        }catch (e: Exception){
+            Result.failure<Weather>(e)
+        }
+        emit(result)
+    }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend  () -> Result<T>) = liveData<Result<T>>(context){
+       val result = try {
+           block()
+       } catch (e: Exception){
+           Result.failure<T>(e)
+       }
+        emit(result)
+    }
+
+    fun savePlace(place: Place) = PlaceDao.savePlace(place)
+
+    fun getSavedPlace() = PlaceDao.getSavedPlace()
+
+    fun isPlaceSaved() = PlaceDao.isPlaceSaved()
 }
